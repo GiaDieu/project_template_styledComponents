03b257e37289736b30e2d06073f7d2d9edf71ccc
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-29 21:28:44 +0700

    create_InfoElements

---
+++ src/components/infoSection/InfoElements.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const InfoContainer = styled.div`
+  color: #fff;
+  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#010606")};
+
+  @media screen and (max-width: 768px) {
+    padding: 6.25rem 0; // 100px 0px
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  z-index: 1;
+  display: grid;
+  width: 100%;
+  height: 860px;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 1.5rem; // 0 24px;
+  max-width: 1100px;
+`;
+
+export const InfoRow = styled.div`
+  display: grid;
+  grid-auto-columns: minmax(auto, 1fr);
+  align-items: center;
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? `'col2 col1'` : `'col1 col2'`};
+
+  @media screen and (max-width: 768px) {
+    grid-template-areas: ${({ imgStart }) =>
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
+  }
+`;
+
+export const ColumnPrimary = styled.div`
+  margin-bottom: 15px;
+  padding: 0 15px;
+  grid-area: col1;
+`;
+
+export const ColumnSecondary = styled.div`
+  margin-bottom: 15px;
+  padding: 0 15px;
+  grid-area: col2;
+`;
+
+export const TextWrapper = styled.div`
+  max-width: 540px;
+  padding-top: 0;
+  padding-bottom: 3.75rem;
+`;
+
+export const TopLine = styled.p`
+  color: #01bf71;
+  font-size: 1rem;
+  line-height: 1rem;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+`;
+
+export const Heading = styled.h2`
+  margin-bottom: 1.5rem; //24px
+  font-size: 2.5rem; // 48px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#010606")};
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+
+export const Subtitle = styled.p`
+  max-width: 440px;
+  margin-bottom: 35px;
+  font-size: 18px;
+  line-height: 24px;
+  color: ${({ darkText }) => (darkText ? "#010606" : "#fff")};
+`;
+
+export const BtnWrap = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const ImgWrap = styled.div`
+  max-width: 550px;
+  height: 100%;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  margin: 0 0 10px 0;
+  padding-right: 0;
+`;
