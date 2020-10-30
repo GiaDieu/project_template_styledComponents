3573f1368fcb05236aa10ab8cbb389269fd35ec8
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-30 21:52:15 +0700

    create_SocialMediaLogoCom

---
+++ src/components/footer/SocialMediaLogo.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  SocialMedia,
+  SocialMediaWrapper,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcon,
+  SocialIconLink,
+} from "./FooterElements";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const SocialMediaLogo = () => {
+  return (
+    <SocialMedia>
+      <SocialMediaWrapper>
+        <SocialLogo to="/">KAO</SocialLogo>
+        <WebsiteRights>
+          KAO &copy; {new Date().getFullYear()} All Rights reserved.
+        </WebsiteRights>
+        <SocialIcon>
+          <SocialIconLink
+            href="https://www.facebook.com/"
+            target="_blank"
+            aria-label="Facebook"
+          >
+            <FaFacebook />
+          </SocialIconLink>
+          <SocialIconLink
+            href="https://www.instagram.com/"
+            target="_blank"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </SocialIconLink>
+          <SocialIconLink
+            href="https://www.youtube.com/"
+            target="_blank"
+            aria-label="Youtube"
+          >
+            <FaYoutube />
+          </SocialIconLink>
+          <SocialIconLink
+            href="https://twitter.com/"
+            target="_blank"
+            aria-label="Twitter"
+          >
+            <FaTwitter />
+          </SocialIconLink>
+          <SocialIconLink
+            href="https://www.linkedin.com/feed/"
+            target="_blank"
+            aria-label="Linkedin"
+          >
+            <FaLinkedin />
+          </SocialIconLink>
+        </SocialIcon>
+      </SocialMediaWrapper>
+    </SocialMedia>
+  );
+};
+
+export default SocialMediaLogo;
