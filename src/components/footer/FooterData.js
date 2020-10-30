a8273f30c6da3fd1befd5c1a0b5f3e64e725e457
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-30 21:51:43 +0700

    create_FooterData.js

---
+++ src/components/footer/FooterData.js
@@ -0,0 +1,30 @@
+export const objectFooterInfo = {
+  data: [
+    {
+      id: "about",
+      title: "About Us",
+      desc: [
+        "How It Works",
+        "Testimonials",
+        "Careers",
+        "Investors",
+        "Terms of Services",
+      ],
+    },
+    {
+      id: "video",
+      title: "Videos",
+      desc: ["Submit Videos", "Ambassadors", "Agency", "Influencer"],
+    },
+    {
+      id: "contact",
+      title: "Contact Us",
+      desc: ["Contact", "Support", "Destination", "Sponsorship"],
+    },
+    {
+      id: "social",
+      title: "Social Media",
+      desc: ["Instagram", "Facebook", "Youtube", "Twitter"],
+    },
+  ],
+};
