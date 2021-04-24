2bed2a35d78aded6bb5a8afae4363356aa3ddf04
GiaDieu <giadieuly@gmail.com>
2021-04-24 14:24:01 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import Footer from "../components/footer";
-import HeroSection from "../components/heroSection";
-import InfoSection from "../components/infoSection";
+import React, { useState } from 'react';
+import Footer from '../components/footer';
+import HeroSection from '../components/heroSection';
+import InfoSection from '../components/infoSection';
 import {
-  homeObjOne,
-  homeObjTwo,
-  homeObjThree,
-} from "../components/infoSection/Data";
-import Navbar from "../components/navbar";
-import Services from "../components/services";
-import Sidebar from "../components/sidebar";
+    homeObjOne,
+    homeObjTwo,
+    homeObjThree,
+} from '../components/infoSection/Data';
+import Navbar from '../components/navbar';
+import Services from '../components/servicesSection';
+import Sidebar from '../components/sidebar';
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
-      <HeroSection />
-      <InfoSection {...homeObjOne} />
-      <InfoSection {...homeObjTwo} />
-      <Services />
-      <InfoSection {...homeObjThree} />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+            <HeroSection />
+            <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <Services />
+            <InfoSection {...homeObjThree} />
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
